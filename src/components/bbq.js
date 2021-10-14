3372808ba2c3d90ab4f787f1246f3556175e3685
kiaramclarty <89852907+kiaramclarty@users.noreply.github.com>
2021-10-14 12:27:12 -0400

    this is my summary

--- src/components/bbq.js
+++ src/components/bbq.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import bbq from "./images/bbq.jpg";
+import "./css/slice.css";
 
 function Bbq() {
-  return(
-    <div>
-
+  return (
+    <div className="Bbq col-4">
+      <div className="card">
+        <img src={Bbq} class="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">BBQ Slice</h5>
+          <p className="card-text">
+            {" "}
+            Perhaps the most popular slice we offer BBbq chicken over a
+            delicious dollar pizza. What more could one want in life?
+          </p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Price: $1.00</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
-
 export default Bbq;
